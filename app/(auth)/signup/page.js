3be39d2893318530895c92f93c2555826a004a2d
Hasan Ashrafi <yasashrafi@gmail.com>
2025-09-10 15:10:55 +0330

    Add Signup component to render SignupPage for user registration

---
+++ app/(auth)/signup/page.js
@@ -0,0 +1,11 @@
+import SignupPage from '@/components/auth/SignupPage'
+import Link from 'next/link'
+import React from 'react'
+
+function Signup() {
+    return (
+        <SignupPage />
+    )
+}
+
+export default Signup
