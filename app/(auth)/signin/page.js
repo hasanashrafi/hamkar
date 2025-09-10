0c36f0ac2d3b64b85a89e70d540e8a7ca10aaf58
Hasan Ashrafi <yasashrafi@gmail.com>
2025-09-10 15:10:48 +0330

    Add Signin component to render SigninPage for authentication

---
+++ app/(auth)/signin/page.js
@@ -0,0 +1,11 @@
+import SigninPage from '@/components/auth/SigninPage'
+import React from 'react'
+
+function Signin() {
+   
+    return (
+        <SigninPage />
+    )
+}
+
+export default Signin
