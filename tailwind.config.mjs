c50a305ef4b8a3f1d06acb489b621ef35cda4e6b
Hasan Ashrafi <yasashrafi@gmail.com>
2025-09-10 15:09:21 +0330

    Update font settings in globals.css and add tailwind.config.mjs for custom font configuration

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+// tailwind.config.mjs
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        myFont: ["var(--font-Dana)", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
